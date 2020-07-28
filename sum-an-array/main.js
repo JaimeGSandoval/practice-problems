c788b609016b5169c66c6181555b2b216638601a
Jaime Sandoval <jaimesof82@gmail.com>
2020-07-28 10:14:48 -0700

    Jaime completed the sum an array exercise

---
+++ sum-an-array/main.js
@@ -0,0 +1,17 @@
+// Sum an Array
+
+var numbersArray = [10, 5, 3, 7, 2];
+var numbersArray2 = [1, 2, 3, 4, 5];
+var numbersArray3 = [5, 3, 7, 4];
+
+function sumArray(array) {
+  var sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
+
+sumArray(numbersArray);
+sumArray(numbersArray2);
+sumArray(numbersArray3);
