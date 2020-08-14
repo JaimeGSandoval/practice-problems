387929d04f80108f1fea0deb11c8257a8733fdf2
jaimegsandoval <jaime.devs@gmail.com>
2020-08-14 13:18:46 -0700

    Figured out how to solve it without the sort method!

--- payscale-analyzer/main.js
+++ payscale-analyzer/main.js
@@ -1,30 +1,30 @@
 // Define a function named payscaleAnalyzer which takes in an array of hourly wages and returns the numeric difference between the highest and lowest wages
 
 
-//Couldn't get this way to work
-// function payScaleAnalyzer(payList) {
-//   var highest = payList[0];
-//   var lowest = payList[0];
-//   for (var i = 0; i < payList.length; i++) {
-//     if (payList[i] > highest) {
-//       payList[i] = highest;
-//     }
-//   }
-//   for (var x = 0; x < payList.length; x++) {
-//     if (payList[x] < lowest) {
-//       lowest = payList[x];
-//     }
-//   }
-// return highest - lowest
-
-// }
-
 function payScaleAnalyzer(payList) {
-  payList.sort();
+  var highest = payList[0];
+  var lowest = payList[0];
+  for (var i = 0; i < payList.length; i++) {
+    if (payList[i] > highest) {
+      highest = payList[i];
 
-  return payList[payList.length - 1] - payList[0];
+    }
+  }
+  for (var x = 0; x < payList.length; x++) {
+    if (payList[x] < lowest) {
+      lowest = payList[x];
+    }
+  }
+  return highest - lowest
 
 }
+
+// function payScaleAnalyzer(payList) {
+//   payList.sort();
+
+//   return payList[payList.length - 1] - payList[0];
+
+// }
 
 payScaleAnalyzer([28, 34, 21, 39])   // -> 18
 payScaleAnalyzer([18, 22, 19, 26])   // -> 8
