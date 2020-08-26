84b2591b0476dcb766b278f7188b882c4a2d2a30
jaimegsandoval <jaime.devs@gmail.com>
2020-08-26 10:39:39 -0700

    Added Uzairs solutution

--- account-number-protector/main.js
+++ account-number-protector/main.js
@@ -4,16 +4,28 @@
 // Return Value
 // The last four numbers of the credit card number with "**" as a prefix
 
-function accountNumberProtector(cardNumberString) {
+// function accountNumberProtector(cardNumberString) {
+//   let prefixAccNum = '**';
+//   let last4Num = cardNumberString.slice(12);
+//   for (let i = cardNumberString - 4; i < last4Num.length; i++) {
+//     prefixAccNum += last4Num[i];
+//   }
+//   return prefixAccNum;
+
+// }
+
+
+// Uzair's solution
+function accountNumberProtector2(cardNumberString) {
+
   let prefixAccNum = '**';
-  let last4Num = cardNumberString.slice(12);
-  for (let i = 0; i < last4Num.length; i++) {
-    prefixAccNum += last4Num[i];
+  for (let i = cardNumberString.length - 4; i < cardNumberString.length; i++) {
+    prefixAccNum += cardNumberString[i];
   }
   return prefixAccNum;
 
 }
 
-accountNumberProtector("5543223485638832") // -> "**8832"
-accountNumberProtector("8934235477210943") // -> "**0943"
-accountNumberProtector("7513635499320192") // -> "**0192"
+accountNumberProtector2("5543223485638832") // -> "**8832"
+accountNumberProtector2("8934235477210943") // -> "**0943"
+accountNumberProtector2("7513635499320192") // -> "**0192"
