9e7bf6ea3a9224bbadcf64546b9ba2f8a6612c84
jaimegsandoval <jaime.devs@gmail.com>
2020-11-12 16:33:13 -0800

    Completed problem exercise

---
+++ triangle-num-sequence/main.js
@@ -0,0 +1,11 @@
+function triangle(n) {
+  return n * (n + 1) / 2;
+}
+
+// function triangle(n) {
+//   var count = 0;
+//   for (var num = 0; num <= n; num++) {
+//     count = count + num;
+//   }
+//   return count
+// }
