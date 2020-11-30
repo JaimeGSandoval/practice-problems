65d77419470f8113b6ab919def7b64f7907d6b9d
jaimegsandoval <jaime.devs@gmail.com>
2020-11-29 23:06:18 -0800

    Completed practice exercise

---
+++ make-circle/main.js
@@ -0,0 +1,31 @@
+class Rectangle {
+  constructor(sideA, sideB) {
+    this.sideA = sideA;
+    this.sideB = sideB;
+  }
+  getArea() { return this.sideA * this.sideB }
+  getPerimeter() { return (this.sideA + this.sideB) * 2 }
+}
+
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  getPerimeter() {
+    return 2 * Math.PI * this.radius;
+  }
+
+  getArea(radius) {
+    return Math.PI * Math.pow(this.radius, 2);
+  }
+}
+
+class Circle {
+  constructor(r) {
+    this.r = r;
+  }
+  getArea = () => Math.PI * Math.pow(this.r, 2);
+  getPerimeter = () => 2 * Math.PI * this.r;
+}
