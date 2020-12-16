62d38510bf56a0e176b44a51a22b49aa23d55fc6
jaimegsandoval <jaime.devs@gmail.com>
2020-12-15 16:51:47 -0800

    Added revealing module pattern

--- js-patterns/module-pattern.js
+++ js-patterns/module-pattern.js
@@ -57,3 +57,64 @@ MYAPP3.utilities.array = (function () {
   }
 
 }());
+
+
+
+// Create REVEALING Module Pattern
+
+
+// Set a namespace
+const MYAPP4 = {};
+
+MYAPP4.namespaceObj = function (ns_string) {
+  let parts = ns_string.split('.'),
+    parent = MYAPP4;
+
+  // remove the leading "MYAPP" global from the parts array
+  if (parts[0] === 'MYAPP4') {
+    parts = parts.slice(1);
+  }
+
+  for (let i = 0; i < parts.length; i += 1) {
+
+    // Create a property if it doesn't exist
+    if (typeof parent[parts[i]] === 'undefined') {
+      parent[parts[i]] = {};
+    }
+    parent = parent[parts[i]];
+  }
+  return parent;
+}
+
+
+
+// Create a namespace object for the module
+MYAPP4.namespaceObj('MYAPP4.utilities.array');
+
+// Create an IFEE for privacy if needed and returns an OBJECT, a MODULE with the actual public interface
+MYAPP4.utilities.array = (function () {
+  // Private properties
+  let array_string = '[object Array]',
+    ops = Object.prototype.toString(),
+
+    // Private methods
+    inArray = function (haystack, needle) {
+      for (let i = 0, max = haystack.length; i < max; i += 1) {
+        if (haystack[i] === needle) {
+          return i;
+        }
+      }
+      return -1;
+    },
+
+    isArray = function (a) {
+      return ops.call(a) === array_string;
+    };
+  // end of variable definitions
+
+  // REVEALING Public API
+  return {
+    isArray: isArray,
+    indexOf: inArray
+  }
+}());
