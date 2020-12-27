c72955b2d812059a0f78bf91ee516eebd7fbe6a4
jaimegsandoval <jaime.devs@gmail.com>
2020-12-27 14:56:39 -0800

    Added "use strict" and a new callback pattern example

--- js-object-creation-patterns/callback-pattern.js
+++ js-object-creation-patterns/callback-pattern.js
@@ -1,4 +1,7 @@
+"use strict";
+
 // Callback pattern
+
 let findNodes = function (callback) {
   let i = 10, // big heavy loop
     nodes = [], // store the result
@@ -30,3 +33,34 @@ let chidori = function (node) {
 };
 
 findNodes(chidori);
+
+
+const pullScores = function (obj, section = 1, callback) {
+  let scores = {};
+
+  for (let key in obj.scores) {
+    if (key.includes('s' + section)) {
+      let val = obj.scores[key];
+      if (typeof callback === 'function') {
+        val = callback(val);
+      }
+      scores[key] = val;
+    }
+  }
+  return scores;
+};
+
+let newObj = pullScores(learner1, 1, function (val) {
+  if (val === null) {
+    return 0;
+  } else {
+    let arr = val.split(':');
+    return arr[0] / arr[1] * 100;
+  }
+});
+
+
+// Advantages of Callback pattern
+// Helps to not have repeating code. You use a generic function but pass in different callback functions to process the value instead of redoing things for each one of those. You simply pass a different function if you want to do something different
+// Makes you function more generic and versatile so you can handle several types of functionality
+// Makes code more maintainable. One only have to deal with code in the generic function, making it nore maintainable
---
+++ js-object-creation-patterns/learner1.js
@@ -0,0 +1,19 @@
+"user strict";
+
+let learner1 = {
+  firstName: 'Uzumaki',
+  lastName: 'Naruto',
+  scores: {
+    s1q1: '5:8',
+    s1q2: '9:10',
+    s1q3: null,
+    s1q4: '1:5',
+    s2q1: '6:8',
+    s2q2: null,
+    s2q3: '4:20',
+    s2q4: null,
+    s2q5: null,
+    s3q1: '6:10',
+    s3q3: '7:7'
+  }
+};
