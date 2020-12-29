510c40d7fbbb7f6c3182fab6564c70dcc6cf2c15
jaimegsandoval <jaime.devs@gmail.com>
2020-12-28 23:28:43 -0800

    Added different version of singleton pattern

--- js-design-patterns/singleton.js
+++ js-design-patterns/singleton.js
@@ -32,3 +32,46 @@ const Singleton = (function () {
 
 const userProcess = Singleton.getProcessManager();
 const userProcess2 = Singleton.getProcessManager();
+
+
+// Example 2
+class Settings {
+  constructor() {
+
+    // This checks to see if all of the code below has run, making the instance the one and only one of it. If it is, just return it.
+    if (Settings.instance instanceof Settings) {
+      return Settings.instance;
+    }
+
+    this.settingsObject = {
+      'background': '#ff0000',
+      'version': Math.floor(Math.random() * 4000)
+    };
+
+    // Freeze the settings object to stop people from modifying it after it's created
+    Object.freeze(this.settingsObject);
+
+    // Below is how to create a singleton.
+    // Whe you keep calling 'new Settings', each time one going to get the exact same Settings instance
+
+    // Object.freeze means that people can't modify the instance which we create, such as adding, deleting, or changing properties or methods.
+    Object.freeze(this);
+
+    // This applies to he Settings class, not the instance that was just created. This is key to getting the singleton to work. This is a semi global property on the Settings class.
+    Settings.instance = this;
+  }
+
+  getInstance(key) {
+    return this.settingsObject[key];
+  }
+}
+
+  Settings.prototype.test = () => {
+  console.log('bankai!!');
+}
+
+const setting1 = new Settings();
+const setting2 = new Settings();
+
+// This is true. Both point to the ONE and ONLY instance.
+setting1 === setting2;
