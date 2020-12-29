6db32823f91de4ca8984727433958fbdee5b48c7
jaimegsandoval <jaime.devs@gmail.com>
2020-12-28 22:38:08 -0800

    Added a new singleton pattern

--- js-design-patterns/singleton.js
+++ js-design-patterns/singleton.js
@@ -1,23 +1,34 @@
-//  ____________
-// |            |
-// |   Object A | <--------
-// |____________|          |
-//                         |
-//                         |
-//                         |
-//                         V
-//                 __________________________________________
-//                 |               SINGLETON                 |
-//                 |      _____________________________      |
-//                 |     |                             |     |
-//                 |     |       SHARED RESOURCE       |     |
-//                 |     |  ________________________   |     |
-//                 |     | |     STORED STATE       |  |     |
-//                 |     | |________________________|  |     |
-//                 |     |_____________________________|     |
-//                 |_________________________________________|
-//                         /\
-//  ____________           |
-// |            |          |
-// | Object B   | <--------|
-// |____________|
+"use strict";
+
+// Singleton pattern is user when you only want a single instance of a class
+// There are many ways to do it
+
+function Process(state) {
+  this.state = state;
+}
+
+const Singleton = (function () {
+  function ProcessManager() {
+    this.numProcess = 0;
+  }
+
+  let pManager;
+
+  function createProcessManager() {
+    pManager = new ProcessManager();
+    return pManager;
+  }
+
+  return {
+    getProcessManager: () => {
+      if (!pManager) {
+        pManager = createProcessManager();
+        return pManager;
+      }
+      return pManager;
+    }
+  }
+}());
+
+const userProcess = Singleton.getProcessManager();
+const userProcess2 = Singleton.getProcessManager();
