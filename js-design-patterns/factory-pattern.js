5eb8246a6f8d9c29129fd4e847e5f3e044d4c0f7
jaimegsandoval <jaime.devs@gmail.com>
2020-12-29 00:13:38 -0800

    Added new factory pattern

--- js-design-patterns/factory-pattern.js
+++ js-design-patterns/factory-pattern.js
@@ -41,3 +41,45 @@ const greet2 = new Greeting2('Yo');
 const greet3 = greeting3('What up');
 
 document.querySelector('button').addEventListener('click', greet3.greet);
+
+
+// Example 2
+
+function Developer(name) {
+  this.name = name;
+  this.type = 'Developer';
+}
+
+function Tester(name) {
+  this.name = name;
+  this.type = 'Tester';
+}
+
+
+function EmployeeFactory() {
+  this.create = (name, type) => {
+    switch (type) {
+      case 1:
+        return new Developer(name);
+        break;
+      case 2:
+        return new Tester(name);
+        break;
+    }
+  }
+}
+
+
+function say() {
+  console.log('Hi, I am ' + this.name + ' and I am a ' + this.type);
+}
+
+const employeeFactory = new EmployeeFactory();
+const employees = [];
+
+employees.push(employeeFactory.create('Darth Vader', 1));
+employees.push(employeeFactory.create('Luke Skywalker', 2));
+
+employees.forEach(employee => {
+  say.call(employee);
+})
