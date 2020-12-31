91523d9381f1a25cf988ba8fe5636dda912ec5c9
jaimegsandoval <jaime.devs@gmail.com>
2020-12-30 19:17:04 -0800

    Completed the exercise

--- regexp/main.js
+++ regexp/main.js
@@ -1,3 +1,8 @@
 const timeFinder = /\d{ 2}:\d{ 2}/;
 const emptyString = /^$/;
 const characterClass = /\S\?$/;
+
+// Remove all vowels with regular expressions
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, '');
+}
