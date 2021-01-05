649b40dc22d32cb1c50b475136c08999dcba054d
jaimegsandoval <jaime.devs@gmail.com>
2021-01-04 16:00:12 -0800

    Added correct initializer value for for loop

--- order-by-num-in-str/main.js
+++ order-by-num-in-str/main.js
@@ -1,7 +1,7 @@
 function order(words) {
   var array = words.split(' ');
   var sortedArray = [];
-  for (i = 0; i <= array.length; i++) {
+  for (i = 1; i <= array.length; i++) {
     for (j = 0; j < array.length; j++) {
       if (array[j].indexOf(i) >= 0) {
         sortedArray.push(array[j]);
