10901c7c56b36380125433ad336076284400ed14
jaimegsandoval <jaime.devs@gmail.com>
2021-01-06 23:40:48 -0800

    Reworked the example

--- order-by-num-in-str/main.js
+++ order-by-num-in-str/main.js
@@ -1,16 +1,20 @@
 function order(words) {
   var array = words.split(' ');
   var sortedArray = [];
-  for (i = 1; i <= array.length; i++) {
+  for (i = 0; i <= array.length; i++) {
     for (j = 0; j < array.length; j++) {
+      console.log(array[j])
       if (array[j].indexOf(i) >= 0) {
+        console.log(array[j].indexOf(i))
         sortedArray.push(array[j]);
       }
     }
   }
   return sortedArray.join(' ');
 }
+const string = 'tes4t 2test te3st 1test test5';
 
+console.log(order(string));
 
 function order2(words) {
   return words.split(' ').sort((wordA, wordB) => wordA.match(/\d+/) > wordB.match(/\d+/)).join(' ')
