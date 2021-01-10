eb29c32b74d3298bbcaba7448e6f0975c8e02b3f
jaimegsandoval <jaime.devs@gmail.com>
2021-01-09 19:12:37 -0800

    Added more facade notes

--- js-design-patterns/facade-pattern.js
+++ js-design-patterns/facade-pattern.js
@@ -70,3 +70,36 @@ function getFetch(url, params = {}) {
 //     params: params
 //   }).then(res => res.data)
 // }
+
+// Example 2
+// Here the facade pattern is combined with the module pattern
+
+const module = (function () {
+  let _private = {
+    i: 5,
+    get: function () {
+      console.log('current value:' + this.i);
+    },
+    set: function (val) {
+      this.i = val;
+    },
+    run: function () {
+      console.log('running');
+    },
+    jump: function () {
+      console.log('jumping')
+    }
+  };
+
+  return {
+    facade: function (args) {
+      _private.set(args.val);
+      _private.get();
+      if (args.run) {
+        _private.run();
+      }
+    }
+  };
+})();
+
+module.facade({ run: true, val: 10 });
