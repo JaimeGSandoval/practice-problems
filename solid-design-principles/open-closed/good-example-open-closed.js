0f6edae75cbbaf5411223e6424ce318db73b3558
jaimegsandoval <jaime.devs@gmail.com>
2021-01-10 23:32:56 -0800

    Added solid examples

---
+++ solid-design-principles/open-closed/good-example-open-closed.js
@@ -0,0 +1,66 @@
+class BooleanQuestion {
+  constructor(description) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log('1. True');
+    console.log('2. False');
+  }
+}
+
+
+class MultipleChoiceQuestions {
+  constructor(description, options) {
+    this.description = description;
+    this.options = options;
+  }
+
+  printQuestionChoices() {
+    this.options.forEach((option, index) => {
+      console.log(`${index + 1}. ${option}`)
+    })
+  }
+}
+
+class TextQuestion {
+  constructor(description, options) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log('Answer: ____________');;
+  }
+}
+
+class RangeQuestion {
+  constructor(description) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log('Minimum: ____________');
+    console.log('Maximum: ____________');
+  }
+}
+
+
+// Closed but opened to be extended. You can add another class and question and not have to change this to have it display.
+function printQuiz(questions) {
+  questions.forEach(question => {
+    console.log(question.description)
+    question.printQuestionChoices();
+    console.log('');
+  })
+}
+
+const questions = [
+  new BooleanQuestion('This video is useful'),
+  new MultipleChoiceQuestions('Who is you favorite Hokage?', ['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh']),
+  new TextQuestion('Describe your favorite Jutsu'),
+  new RangeQuestion('What\'s the kunai limit in your village?')
+];
+
+printQuiz(questions);
+
+// When you see those big switch statements or big if statements, always realize that your almost always violating the open closed principle and you should always refactor that code to use for example classes, or functions, or modules, something that breaks out that logic into individual classes or functions that have the logic in them, and then that crazy switch statement can be broken down into one single function call
