dbd370d138de91d82a778528fa472d745efe4d2c
jaimegsandoval <jaime.devs@gmail.com>
2021-01-21 15:59:10 -0800

    Added import export examples

---
+++ import-export-examples/controller.js
@@ -0,0 +1,13 @@
+/// DEFAULT EXPORT
+const defaultController = {
+  message: 'I\'m the Default Controller',
+}
+
+export default defaultController;
+
+// EXPORT
+const controller = {
+  message: 'I\'m the Controller',
+}
+
+export { controller };
---
+++ import-export-examples/model.js
@@ -0,0 +1,13 @@
+// EXPORT DEFAULT
+const defaultModel = {
+  message: 'I\'m the Default Model'
+}
+
+export default defaultModel;
+
+// EXPORT
+const model = {
+  message: 'I\'m the Model'
+}
+
+export { model };
