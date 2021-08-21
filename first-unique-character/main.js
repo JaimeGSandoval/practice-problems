27c776761fb4a2e8f0784e51d936ba38704d3967
jaimegsandoval <jaime.devs@gmail.com>
2021-08-20 22:33:07 -0700

    added new solution

--- first-unique-character/main.js
+++ first-unique-character/main.js
@@ -1,48 +1,62 @@
-"use strict";
+// "use strict";
 
-function firstUniqChar(str) {
-  const hash = {};
-  for (const char of str) {
-    if (hash[char]) {
-      hash[char]++;
-    }
-    else {
-      hash[char] = 1;
+// function firstUniqChar(str) {
+//   const hash = {};
+//   for (const char of str) {
+//     if (hash[char]) {
+//       hash[char]++;
+//     }
+//     else {
+//       hash[char] = 1;
+//     }
+//   }
+
+//   for (const letter in hash) {
+//     if (hash[letter] === 1) {
+//       return str.indexOf(letter);
+//     }
+//   }
+
+//   return -1;
+// };
+
+// const test = 'qqlllooddw';
+
+// // Example 2 Optimized version
+
+// function firstUniqChar2(str) {
+//   const hash = {};
+//   for (let i = 0; i < str.length; i++) {
+//     const char = str[i];
+//     if (hash[char]) {
+//       hash[char][0]++;
+//     }
+//     else {
+//       hash[char] = [1, i];
+//     }
+//   }
+
+//   for (const letter in hash) {
+//     if (hash[letter][0] === 1) {
+//       return hash[letter][1];
+//     }
+//   }
+//   return -1;
+// };
+
+// const test2 = 'qqlllooddw';
+
+const specialNumber = (n) => {
+  let test = n.toString().split('');
+  console.log(test);
+  for (let i = 0; i < test.length; i++) {
+    const temp = Number(test[i]);
+    if (temp > 5) {
+      console.log(test[i]);
+      return 'NOT!!';
     }
   }
-
-  for (const letter in hash) {
-    if (hash[letter] === 1) {
-      return str.indexOf(letter);
-    }
-  }
-
-  return -1;
+  return 'Special!!';
 };
 
-const test = 'qqlllooddw';
-
-
-// Example 2 Optimized version
-
-function firstUniqChar2(str) {
-  const hash = {};
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    if (hash[char]) {
-      hash[char][0]++;
-    }
-    else {
-      hash[char] = [1, i];
-    }
-  }
-
-  for (const letter in hash) {
-    if (hash[letter][0] === 1) {
-      return hash[letter][1];
-    }
-  }
-  return -1;
-};
-
-const test2 = 'qqlllooddw';
+console.log(specialNumber(11));
