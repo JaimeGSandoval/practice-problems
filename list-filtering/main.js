ac65a19f240bf487636fd843582dbc9d8cef343a
jaimegsandoval <jaime.devs@gmail.com>
2021-08-22 18:13:01 -0700

    completed another exercise

--- list-filtering/main.js
+++ list-filtering/main.js
@@ -1,2 +1,3 @@
 const filter_list(l) = l.filter((v) => typeof v == 'number');
 const accum = (s) => s.split('').map((letter, i) => `${letter.toUpperCase()}${letter.toLowerCase().repeat(i)}`).join('-');
+const isSquare = n => Math.sqrt(n) * Math.sqrt(n);
